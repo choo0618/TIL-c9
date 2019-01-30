bdb65a1abab40024ca92325b38c913ebde6a9fef
Hyeon Jin <choo0618@naver.com>
2019-01-30 08:50:35 +0000

    D21 | 190130 PM | 1:N Relation

---
+++ django/crud-plus/questions/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+            
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+    votes = models.IntegerField()
